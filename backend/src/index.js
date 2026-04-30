521f156baba8e7cdda2ed7c0e6805af08ebbcc87
Akash-Deep-77 <142450928+Akash-Deep-77@users.noreply.github.com>
2026-05-01 02:53:40 +0530

    mongodb atlas connection through compass

---
+++ backend/src/index.js
@@ -0,0 +1,58 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+import dns from 'dns';
+// Change DNS to resolve 'ECONNREFUSED' error caused by SRV format
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app = express();
+// ( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("Error: ", error); 
+//             throw error        
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+//     } catch (error) {
+//         console.error("Error: ", error);  
+//         throw error     
+//     }
+// })()
